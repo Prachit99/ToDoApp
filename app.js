28803a9a345e38e0fe6b59f8b0ad23172f786f92
Prachit99 <prachitmhalgi@gmail.com>
2021-02-19 22:52:45 +0530

    added materialize css

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
+const Task = require('./models/tasks')
 const express = require('express');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 const app = express();
+app.set('view engine', 'ejs')
+
+const mongoDB = "mongodb://localhost:27017/ToDoApp";
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
+    if (err)
+        console.error(err);
+    else
+        console.log("Connected to the mongodb"); 
+});
 
 
-app.use('view engine', 'ejs');
-app.listen(3000);
+const jsonParser = bodyParser.json()
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+
+app.listen(3000, () => {
+    console.log("Server has started")
+});
 
 app.get('/', (req, res) => {
     res.render('index');
--- models/tasks.js
+++ models/tasks.js
@@ -3,14 +3,14 @@ const Schema = mongoose.Schema;
 
 const taskSchema = new Schema({
     name:{
-        type:String,
-        required:true
+        type: String,
+        require: true
     },
-    done:{
-        type: Boolean,
-        required:true
+    date:{
+        type: Date,
+        default: Date.now
     }
 });
 
-const Task = mongoose.model('Task',taskSchema)
+const Task = mongoose.model('Task', taskSchema)
 module.exports = Task;
